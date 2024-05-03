f1a080d26af6b2b2d81b36679e513148573ff483
marciojrdev1998 <marciojrgalvao@gmail.com>
2024-05-03 00:48:13 -0300

    smart-tv-aula

---
+++ SISTEMA-SMART-TV/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminurVolume();
+        smartTv.diminurVolume();
+        smartTv.diminurVolume();
+        smartTv.diminurVolume();
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+        
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+    }
+}
---
+++ TIPOS VARIAVEIS/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.00; // Valor do salário mínimo
+
+        short numeroCurto = 1; // Um número curto (short é um tipo de dado de 16 bits)
+        int numeroNormal = numeroCurto; // Converte um short para int
+        short numeroCurto2 = (short) numeroNormal; // Converte um int para short (casting)
+
+        // Correção: Declaração de variável sem espaços ou números no nome
+        int numero = 5;
+
+        numero = 10; // Atribuição de valor à variável
+
+        System.out.print(numero); // Imprime a variável 'numero'
+
+        final double VALOR_DE_PI = 3.14;
+        
+    }
+}
